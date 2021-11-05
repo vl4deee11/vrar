270983079ebcc3d710b685ce196f5f7975473f86
vl4deee11 <boi4enkovlad@yandex>
2021-11-05 15:37:14 +0300

    added ind1

---
+++ Ind1/app/src/main/java/com/google/codelabs/findnearbyplacesar/ar/PlaceNode.kt
@@ -0,0 +1,44 @@
+package com.google.codelabs.findnearbyplacesar.ar
+
+import android.content.Context
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.codelabs.findnearbyplacesar.R
+import com.google.codelabs.findnearbyplacesar.model.Place
+
+class PlaceNode(
+    val context: Context,
+    val height: Int,
+    val width: Int,
+    val place: Place?
+) : Node() {
+
+    private var placeRenderable: ViewRenderable? = null
+
+    override fun onActivate() {
+        super.onActivate()
+
+        if (scene == null) {
+            return
+        }
+
+        if (placeRenderable != null) {
+            return
+        }
+
+        val imageWidth = height / 100f
+        val imageHeight = width / 100f
+        this.localScale = Vector3(imageWidth, imageHeight, imageWidth)
+
+        ViewRenderable.builder()
+            .setView(context, R.layout.place_view)
+            .setVerticalAlignment(ViewRenderable.VerticalAlignment.CENTER)
+            .build()
+            .thenAccept {
+                it.isShadowCaster = false
+                renderable = it
+                placeRenderable = it
+            }
+    }
+}
